302de2dffea9b8c6eb4eb72b85064d70c35e2f1e
xuejiao, wang <354798650@qq.com>
2022-08-14 19:54:19 +0800

    test library

---
+++ jenkinsfiles/demo1.groovy
@@ -0,0 +1,27 @@
+#!/usr/bin/env groovy
+
+@Library('pipeline-library-demo')_
+
+import com.qikqiak.GlobalEnv
+import com.qikqiak.GlobalVars
+
+
+
+pipeline {
+    agent any 
+
+    stages{
+        stage('demo global vars') {
+            steps {
+                echo 'Hello world'
+                sayHi '阳明'
+                println GlobalVars.foo
+            }
+        }
+
+        stage('demo function calls') {
+            d g =  new GlobalEnv(this)
+            println g.login()
+        }
+    }
+}
---
+++ src/com/qikqiak/GlobalEnv.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+
+package com.qikqiak
+
+class GlobalEnv implements Serializable  {
+    def steps
+
+    GlobalEnv(steps) {
+        this.steps = steps
+    }
+
+    def login() {
+        if (fileExists("a.txt")) {
+            steps.sh "ls -alt a.txt"
+        } else {
+            steps.sh "echo file a.txt not exits"
+        }
+    }
+}
+
+// make a test
